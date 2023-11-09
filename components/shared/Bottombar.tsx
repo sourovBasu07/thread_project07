5fdc2450f5f73d7bdf34f40dbbde2aeffc2a87b8
sourovBasu07 <souravbasu123456@gmail.com>
2023-11-10 01:03:44 +0600

    first commit

---
+++ components/shared/Bottombar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import { sidebarLinks } from "@/constants";
+
+export default function Bottombar() {
+  const pathname = usePathname();
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link: any) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                {link.label.split(/\s+/)[0]}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
